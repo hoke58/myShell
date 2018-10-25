5e63bf33ae2957580ef0f2e882dec8c9804f31f1
回手掏 <1078424962@qq.com>
2018-10-26 00:46:10 +0800

    Upload MongoFastchain_Backup.sh version_backup.sh app_start.sh greenlight.sh app_stop.sh MongoCert_Backup.sh

---
+++ CCB-SelfService/20181124/app_start.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+######################脚本注释#############################
+# 文件名： app_start.sh[自服务启动脚本]                    #
+# 功  能： 检查应用进程是否正常                           #
+# 作  者： hoke                                           #
+# 时  间： 20181025                                       #
+# 单  元：  BTFL3_AP、BTFWB_                              #
+###########################################################
+BASH_PATH=/home/ap/blockchain/bin/usr
+APP_NAMES=$1
+if [ ! $APP_NAMES ];then
+    bash ${BASH_PATH}/BTF*.sh start
+    if [ $? -eq 0 ];then
+        echo "INFO:[`hostname`][`date +%Y-%m-%d_%H:%M:%S`]服务启动成功"
+        echo "----------------------------------------------------------------------"
+        exit 0
+    else
+        echo "ERROR:[`hostname`][`date +%Y-%m-%d_%H:%M:%S`]服务启动失败"
+        echo "----------------------------------------------------------------------"
+        exit 1
+    fi
+else 
+    bash ${BASH_PATH}/BTF*.sh start ${APP_NAMES}
+    if [ $? -eq 0 ];then
+        echo "INFO:[`hostname`][`date +%Y-%m-%d_%H:%M:%S`]${APP_NAMES}服务启动成功"
+        echo "----------------------------------------------------------------------"
+        exit 0
+    else
+        echo "ERROR:[`hostname`][`date +%Y-%m-%d_%H:%M:%S`]${APP_NAMES}服务启动失败"
+        echo "----------------------------------------------------------------------"
+        exit 1
+    fi
+    echo "----------------------------------------------------------------------"
+fi
+exit 0
