40b400e18473057d9c0ad3bf22b3aebca4466f3c
hoke58 <hoke58@qq.com>
2018-10-26 12:43:08 +0800

    [更新]自服务启动脚本入参支持大小写

--- CCB-SelfService/20181124/app_start.sh
+++ CCB-SelfService/20181124/app_start.sh
@@ -22,109 +22,109 @@ if [ ! $DEPLOY_UNIT ];then
     res=$?
     verifyResult $res "服务"
     echo "----------------------------------------------------------------------"
-    echo "INFO:[`hostname`][`date +%Y-%m-%d_%H:%M:%S`]服务启动成功"
+    echo "INFO:[`hostname`][`date +%Y-%m-%d_%H:%M:%S`]应用启动脚本运行成功"
     echo "----------------------------------------------------------------------"
     exit 0
 else
     case $DEPLOY_UNIT in
-        BTFL3_AP|BTFL3)
+        BTFL3_AP|BTFL3|btfl3)
         bash ${BASH_PATH}/BTFL3_AP_SERVICE.sh start
         res=$?
         verifyResult $res "金融应用服务"
         ;;
-        BTFN1_AP)
+        BTFN1_AP|BTFN1|btfn1)
         bash ${BASH_PATH}/BTFN1_AP_SERVICE.sh start
         res=$?
         verifyResult $res "区块链队列"
         ;;
-        BTFN2_AP)
+        BTFN2_AP|BTFN2|btfn2)
         bash ${BASH_PATH}/BTFN2_AP_SERVICE.sh start
         res=$?
         verifyResult $res "区块链排序"
         ;;
-        BTFN3_AP)
+        BTFN3_AP|BTFN3|btfn3)
         bash ${BASH_PATH}/BTFN3_AP_SERVICE.sh start
         res=$?
         verifyResult $res "区块链对等"
         ;;
-        BTFN4_AP)
+        BTFN4_AP|BTFN4|btfn4)
         bash ${BASH_PATH}/BTFN4_AP_SERVICE.sh start
         res=$?
         verifyResult $res "区块链仲裁"
         ;;
-        BTFN5_AP)
+        BTFN5_AP|BTFN5|btfn5)
         bash ${BASH_PATH}/BTFN5_AP_SERVICE.sh start
         res=$?
         verifyResult $res "Mong-shard1"
         ;;
-        BTFN6_AP)
+        BTFN6_AP|BTFN6|btfn6)
         bash ${BASH_PATH}/BTFN6_AP_SERVICE.sh start
         res=$?
         verifyResult $res "Mongo-config"
         ;;
-        BTFN7_AP)
+        BTFN7_AP|BTFN7|btfn7)
         bash ${BASH_PATH}/BTFN7_AP_SERVICE.sh start
         res=$?
         verifyResult $res "Mongo-router"
         ;;
-        BTFN8_AP)
+        BTFN8_AP|BTFN8|btfn8)
         bash ${BASH_PATH}/BTFN8_AP_SERVICE.sh start
         res=$?
         verifyResult $res "Mongo-shard2"
         ;;
-        BTFN9_AP)
+        BTFN9_AP|BTFN9|btfn9)
         bash ${BASH_PATH}/BTFN9_AP_SERVICE.sh start
         res=$?
         verifyResult $res "Mongo-shard3"
         ;;
-        BTFO1_AP)
+        BTFO1_AP|BTFO1|btfo1)
         bash ${BASH_PATH}/BTFO1_AP_SERVICE.sh start
         res=$?
         verifyResult $res "区块链API"
         ;;
-        BTFO2_AP)
+        BTFO2_AP|BTFO2|btfo2)
         bash ${BASH_PATH}/BTFO2_AP_SERVICE.sh start
         res=$?
         verifyResult $res "电子保险箱"
         ;;
-        BTFO3_AP)
+        BTFO3_AP|BTFO3|btfo3)
         bash ${BASH_PATH}/BTFO3_AP_SERVICE.sh start
         res=$?
         verifyResult $res "链块加速"
         ;;
-        BTFO4_AP)
+        BTFO4_AP|BTFO4|btfo4)
         bash ${BASH_PATH}/BTFO4_AP_SERVICE.sh start
         res=$?
         verifyResult $res "缓存"
         ;;
-        BTFO5_AP)
+        BTFO5_AP|BTFO5|btfo5)
         bash ${BASH_PATH}/BTFO5_AP_SERVICE.sh start
         res=$?
         verifyResult $res "RabbitMQ"
         ;;
-        BTFWB_AP)
+        BTFWB_AP|BTFWB|btfwb)
         bash ${BASH_PATH}/BTFWB_AP_SERVICE.sh start
         res=$?
         verifyResult $res "Nginx"
         ;;
-        BTFX1_WB)
+        BTFX1_WB|BTFX1|btfx1)
         bash ${BASH_PATH}/BTFX1_WB_SERVICE.sh start
         res=$?
         verifyResult $res "外网HAPROXY"
         ;;
-        BTFX2_WB)
+        BTFX2_WB|BTFX2|btfx2)
         bash ${BASH_PATH}/BTFX2_WB_SERVICE.sh start
         res=$?
         verifyResult $res "内网HAPROXY"
         ;;
         *)
         echo "-------------------------------------------------------------------------------------------------"
-        echo "Invalid arguments 请正确输入应用单元名称     eg: ./app_start.sh BTFL3_AP"
+        echo "Invalid arguments 请正确输入应用单元名称     eg: ./app_start.sh BTFL3"
         echo "-------------------------------------------------------------------------------------------------"
         exit 1
     esac
     echo "----------------------------------------------------------------------"
-    echo "INFO:[`hostname`][`date +%Y-%m-%d_%H:%M:%S`]"$2"服务启动成功"
+    echo "INFO:[`hostname`][`date +%Y-%m-%d_%H:%M:%S`]"$2"应用启动脚本运行成功"
     echo "----------------------------------------------------------------------"
     exit 0
 fi
