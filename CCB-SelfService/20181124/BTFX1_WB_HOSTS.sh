c135554eb00d06c4c385c6a5de658d3ada535b45
root <root@Hoke-xps14.localdomain>
2018-10-28 17:36:40 +0800

    [update]greenlight

---
+++ CCB-SelfService/20181124/BTFX1_WB_HOSTS.sh
@@ -0,0 +1,53 @@
+#!/bin/bash 
+cat << EOF
+######################脚本注释#############################
+# 文件名： BTFX1_WB_HOSTS.sh                              #
+# 功  能： 修改外网Haproxy的hosts[绿灯测试前置执行]       #
+# 作  者： hoke                                           #
+# 时  间： 20181025                                       #
+# 单  元： BTFX1_WB                                       #
+# 备  注： 该脚本需使用[root]权限执行                     #
+###########################################################
+EOF
+URL_LIST="domlc forfaiting factoring"
+WUSHU_F5="10.5.200.77"
+run_code=0
+
+green_check(){
+    local curl_8080=`curl -s http://$URL.blockchain.ccb.com:8080/api/systemVersion? --connect-timeout 1| awk -F'[""]' '{print $4}' | wc -l`
+    local curl_80=`curl -s http://$URL.blockchain.ccb.com:80/api/systemVersion? --connect-timeout 1| awk -F'[""]' '{print $4}' | wc -l`
+    if [ $curl_8080 -eq 1 -o $curl_80 -eq 1 ]; then
+        echo "-------------------------------------------------------------------------------------------------"
+        echo "INFO:[`hostname`][`date +%Y-%m-%d_%H:%M:%S`]\""$URL"\"DNS可以正常解析"
+        echo "-------------------------------------------------------------------------------------------------"
+    else
+        echo "-------------------------------------------------------------------------------------------------"
+        echo "ERROR:[`hostname`][`date +%Y-%m-%d_%H:%M:%S`]"$URL"DNS无法解析,请检查/etc/hosts"
+        echo "`cat /etc/hosts|grep $URL`"
+        echo "-------------------------------------------------------------------------------------------------"
+        let run_code++      
+    fi
+}
+
+#check privilege 
+if [  "$(id -nu)" != "root" ]; then 
+    echo "-------------------------------------------------------------------------------------------------"
+    echo "WARN:[`hostname`][`date +%Y-%m-%d_%H:%M:%S`]请使用[root]权限执行脚本"
+    echo "-------------------------------------------------------------------------------------------------"
+    exit 1
+fi
+for URL in $URL_LIST; do
+    ping $URL.blockchain.ccb.com -c 1 -W 1
+    res=$?
+    if [ $res -eq 2 ]; then
+        cp /etc/hosts /etc/hosts.$(date +%Y%m%d%H%M)
+        echo $WUSHU_F5 $URL.blockchain.ccb.com >> /etc/hosts
+        green_check
+    else
+        ip=`ping $URL.blockchain.ccb.com -c 1 -W 1 |awk -F '[()]' 'NR==1{print $2}'`
+        echo ""$URL".blockchain.ccb.com IP: "$ip""
+        green_check
+    fi
+done
+echo $run_code
+exit $run_code
--- CCB-SelfService/20181124/greenlight.sh
+++ CCB-SelfService/20181124/greenlight.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 ######################脚本注释#############################
 # 文件名： greenlight.sh[绿灯测试]                        #
 # 功  能： 绿灯检测Nginx及应用                            #
@@ -8,9 +8,10 @@
 # 备  注： 运行绿灯脚本前确保[BTFX1_WB]能解析产品域名     #
 ###########################################################
 URL_LIST="domlc forfaiting factoring"
+run_code=0
 
 verifyResult() {
-if [ $res -ne 0 ]; then
+if [ $run_code -ne 0 ]; then
     echo "================== ERROR:[`hostname`][`date +%Y-%m-%d_%H:%M:%S`]"$2" is failed =================="
     echo
     exit 1
@@ -21,25 +22,20 @@ fi
 }
 
 for URL in $URL_LIST; do
-    TMP_FILE="${URL}.$$"
-#    echo $TMP_FILE
     echo "Starting ["$URL"] greenlight test..."
-    curl -s http://$URL.blockchain.ccb.com:8080/api/systemVersion? --connect-timeout 1|awk -F'[""]' '{print $4}' >> ${TMP_FILE}
     echo "Get ["$URL"] Java version..."
-    state=`cat $TMP_FILE`
+    state=`curl -s http://$URL.blockchain.ccb.com:8080/api/systemVersion? --connect-timeout 1|awk -F'[""]' '{print $4}'`
     echo "The ["$URL"] Java version: ["$state"]"
     if [ "$state" = "" ];then
         echo "----------------------------------------------------------------------"
         echo "ERROR:[`hostname`][`date +%Y-%m-%d_%H:%M:%S`]Failed to get the [${URL}] Java version"
         echo "----------------------------------------------------------------------"
-        rm ${TMP_FILE}
-        res=1
+        run_code=`expr $run_code + 1`
     else
         echo "----------------------------------------------------------------------"
         echo "INFO:[`hostname`][`date +%Y-%m-%d_%H:%M:%S`]The "$URL" Java version is ["$state"]"
         echo "----------------------------------------------------------------------"
-        rm ${TMP_FILE}
-        res=0
     fi
 done
-verifyResult $res "Greenlight"
+echo $run_code
+verifyResult $run_code "Greenlight"
